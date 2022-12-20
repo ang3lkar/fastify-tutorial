4282a0d043612b1a42f78c28c3f6deb75d66f711
Angelos Karagkiozidis <angelkar@gmail.com>
2022-12-20 15:36:10 +0200

    feat: add update item

--- routes/items.js
+++ routes/items.js
@@ -1,73 +1,96 @@
-const {getItems, getItem, addItem, deleteItem} = require('../controllers/items');
+const {
+  getItems,
+  getItem,
+  addItem,
+  deleteItem,
+  updateItem,
+} = require("../controllers/items");
 
 // Item schema
 const Item = {
-    type: 'object',
-    properties: {
-      id: { type: 'string' }, // comment out and is not returned
-      name: { type: 'string' }, // can also coarse value
-    },
-  }
+  type: "object",
+  properties: {
+    id: { type: "string" }, // comment out and is not returned
+    name: { type: "string" }, // can also coarse value
+  },
+};
 
 // Options for get all items
 const getItemsOpts = {
-    schema: {
-        response: {
-            200: {
-                type: 'array',
-                items: Item,
-            },
-        },
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item,
+      },
     },
-    handler: getItems
-}
+  },
+  handler: getItems,
+};
 
 const getItemOps = {
-    schema: {
-        response: {
-            200: Item
-        }
+  schema: {
+    response: {
+      200: Item,
     },
-    handler: getItem
-}
+  },
+  handler: getItem,
+};
 
 const addItemOps = {
-    schema: {
-        body: {
-            type: 'object',
-            required: ['name'],
-            properties: {
-                name: {type: 'string'}
-            }
-        },
-        response: {
-            201: Item
-        }
+  schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: { type: "string" },
+      },
     },
-    handler: addItem
-}
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
+};
 
 const deleteItemOps = {
-    schema: {
-        response: {
-            204: {
-                type: 'object',
-                message: {
-                    type: 'string'
-                }
-            }
-        }
+  schema: {
+    response: {
+      204: {
+        type: "object",
+        message: {
+          type: "string",
+        },
+      },
     },
-    handler: deleteItem
-}
+  },
+  handler: deleteItem,
+};
+
+const updateItemOps = {
+    schema: {
+      body: {
+        type: "object",
+        required: ["name"],
+        properties: {
+          name: { type: "string" },
+        },
+      },
+      response: {
+        200: Item,
+      },
+    },
+    handler: updateItem,
+  };
 
 function itemRoutes(fastify, options, done) {
-    fastify.get('/items', getItemsOpts);
-    fastify.get('/items/:id', getItemOps);
-    fastify.post('/items', addItemOps);
-    fastify.delete('/items/:id', deleteItemOps);
+  fastify.get("/items", getItemsOpts);
+  fastify.get("/items/:id", getItemOps);
+  fastify.post("/items", addItemOps);
+  fastify.delete("/items/:id", deleteItemOps);
+  fastify.put("/items/:id", updateItemOps);
 
-    done()
+  done();
 }
 
-module.exports = itemRoutes
+module.exports = itemRoutes;
